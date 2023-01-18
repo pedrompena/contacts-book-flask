e268f2b4dd5049bed705d731b1953689ada57aad
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-01-18 17:46:29 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from routes.contacts import contacts
+from flask_sqlalchemy import SQLAlchemy
+from config import DATABASE_CONNECTION_URI
+
+
+app = Flask(__name__)
+
+app.secret_key = "secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
+app.config['SQLALCHEMY_TRACK_MODIFICAATIONS'] = False
+
+SQLAlchemy(app)
+
+app.register_blueprint(contacts)
